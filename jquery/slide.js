1ac2af83aed185ab7fb6b18835b46923eb757913
fllctrlz <fllctrlzfll@gmail.com>
2017-12-31 15:37:30 -0500

    Main
    Descripsiton

---
+++ jquery/slide.js
@@ -0,0 +1,64 @@
+// 
+$(function()
+{
+	var liWidth = $("#gallery ul li").outerWidth(),
+					speed = 3000,
+					timer = setInterval(auto, speed);
+	
+	// mostara botoes
+	$("section#gallery").hover(
+		function(){		
+			clearInterval(timer);
+			$("section#buttons").stop().fadeIn();
+		},
+		function(){
+			$(this).filter(':animated').stop();
+			
+			timer = setInterval(auto, speed);
+			$("section#buttons").stop().fadeOut();
+		});
+	
+	
+	//proximo
+	$(".next").click(function(e){
+		e.preventDefault();
+		
+		if($("#gallery ul").is(':animated'))
+			return false;
+		
+		$("#gallery ul")
+		.css("width","999%")
+		.animate({left:-liWidth}, function(){		
+			$("#gallery ul li").last().after( $("#gallery ul li").first() );
+			$(this).css({left:0, width:"auto"});
+		});
+	});
+	
+	//voltar
+	$(".prev").click(function(e){
+		e.preventDefault();
+		
+		if($("#gallery ul").is(':animated'))
+			return false;
+		
+		$("#gallery ul li").first().before( $("#gallery ul li").last().css('margin-left', -liWidth) );
+		$("#gallery ul").css("width","999%").animate({left:liWidth}
+		,function(){
+			$("#gallery ul li").first().css('margin-left', 0);
+			$(this).css({left:0, width:"auto"});
+		});
+	});
+	
+	function auto(){
+		$('.next').click();
+	}
+	
+
+
+
+	
+	
+	
+	
+	
+});
